3ec22d2d432f2efddc931006df872bd2f0961b2f
kleb <caleb.nwaeze@yahoo.com>
2021-08-21 02:40:20 +0100

    added files

--- base-home.php
+++ base-home.php
@@ -126,10 +126,38 @@
 	line-height:1.6153em
 }
 
+
+
 .img{
     float:right;
     
-    margin-right: 150px;
+    margin-right: 80px;
+    width:800px;
+    
+}
+
+.img2{
+    float:left;
+    width:15%;
+    height:15%;
+    margin-left: 262px;
+    
+}
+
+.img3{
+    float:right;
+    width:15%;
+    height:15%;
+    margin-right: 390px;
+    
+}
+
+.img4{
+    float:right;
+    width:15%;
+    height:15%;
+    margin-right: 200px;
+    padding-top: 0px;
     
 }
 
@@ -141,6 +169,30 @@
   margin-right: auto;
   width: 960px;
 }
+
+.wrapper {
+	width:100%;
+	overflow:hidden;
+	position:relative
+}
+
+.bg-bot {
+	width:100%;
+	background:url(../images/bg-bot-tail.gif) center top repeat-x #fff;
+	padding:30px 0 55px
+}
+#page3 .bg-bot, #page4 .bg-bot {
+	padding:30px 0 49px
+}
+
+.prev-indent-bot {
+	margin-bottom:10px
+}
+.prev-indent-bot2 {
+	margin-bottom:5px
+}
+
+
 </style>
 </head>
 
@@ -173,11 +225,71 @@
             </div>
             </div>
         </div>
-
+    <section id="content">
         <div class="con">
-             <img src = "base.jpg" class = "img"></img>
+             <!--<img src = "base.jpg" class = "img"></img>
+             <img src = "base2.jpg" class = "img2"></img>
+             <img src = "base3.jpg" class = "img3"></img>
+             <img src = "base4.jpg" class = "img4"></img> -->
+             <img src = "base5.jpg" class = "img">
+
             
         </div>
+
+        <div class="bg-bot">
+            <div class="main">
+                <div class="container_12">
+                        <div class="wrapper">
+                            <article class="grid_4">
+                                <h3 class="prev-indent-bot">About Us</h3>
+                                <p class="prev-indent-bot">This Interior Design Template goes with two pack ages: with PSD source files and without them.</p>
+                                PSD source files are available for free for the registered members of Templates.com. The basic package (without PSD source) is available for anyone without registration. 
+                            </article>
+                            <article class="grid_4">
+                                <h3 class="prev-indent-bot">Testimonials</h3>
+                                <div class="quote">
+                                <p class="prev-indent-bot">At vero eos et accusamus et iusto odio tium voluptatum deleniti atque corrupti quos<br>
+                                    dolores et quas molestias excepturi sint occaecati cupiditate.</p>
+                                <h5>James Reese</h5>
+                                Managing Director </div>
+                            </article>
+                            <article class="grid_4">
+                                <h3 class="prev-indent-bot">What’s New?</h3>
+                                <time class="tdate-1" datetime="2011-08-15"><a class="link" href="#">15.08.2011</a></time>
+                                <p class="prev-indent-bot">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.</p>
+                                <time class="tdate-1" datetime="2011-08-11"><a class="link" href="#">11.08.2011</a></time>
+                                Totam rem aperiam, eaque ipsa quae ab illo inven tore veritatis et quasi architecto. 
+                            </article>
+                        </div>
+                    </div>
+                </div>
+        </div>
+</section>
+
+
+<!--==============================footer=================================-->
+<footer>
+  <div class="main">
+    <div class="container_12">
+      <div class="wrapper">
+        <div class="grid_4">
+          <div>Copyright &copy; <a href="#"></a> All Rights Reserved</div>
+          <div>Design by <a target="_blank" href="http://www.calebdesigns.com/">Caleb Nwaeze</a></div>
+          <!-- {%FOOTER_LINK} -->
+        </div>
+        <div class="grid_4"> <span class="phone-numb"><span>+234</span>8067427072</span> </div>
+        <div class="grid_4">
+          <ul class="list-services">
+            <li><a href="#"></a></li>
+            <li><a class="item-2" href="#"></a></li>
+            <li><a class="item-3" href="#"></a></li>
+            <li><a class="item-4" href="#"></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
     
     </body>
 
